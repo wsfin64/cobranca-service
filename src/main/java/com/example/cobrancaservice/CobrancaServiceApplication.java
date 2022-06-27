d44170d7459cd8a930a9c424ea9b8dd75bc01c7b
Wellington <wsfin64@gmail.com>
2022-06-27 10:08:25 -0300

    criacao do servico de cobranca

---
+++ src/main/java/com/example/cobrancaservice/CobrancaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cobrancaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrancaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cobrancaservice/CobrancaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cobrancaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
